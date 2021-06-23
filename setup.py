d1b155017f37490583d892ecf25a9c18b0f29e09
yzqin <yzqin1996@gmail.com>
2021-06-22 22:38:25 -0700

    search recursively for package resources

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
+import os
+
 from setuptools import setup
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('sapien_viz/assets')
 
 setup(
     install_requires=open("requirements.txt").read(),
+    package_data={'sapien_viz': extra_files},
 )
